4495069f08ed70a7339258b03d13209c7a7b8465
John Martin <john.martin@cleardata.com>
2016-08-03 13:02:09 -0500

    fix flags

--- sec-update.sh
+++ sec-update.sh
@@ -2,5 +2,5 @@ yum list-sec > /tmp/updates.txt
 
 for ITEM in $(awk -F' ' '{print $1}' /tmp/updates.txt|egrep 'FEDORA')
 do 
-    yum update -y advisory ${ITEM}
+    yum update -y --advisory ${ITEM}
 done
